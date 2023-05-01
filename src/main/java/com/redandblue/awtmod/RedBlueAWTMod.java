97fcbdcbd1bfe76d327d3064a89d8bc2995db620
camberospabloisc <isc.pablocamberos@gmail.com>
2023-04-30 18:12:23 -0600

    Primer commit

---
+++ src/main/java/com/redandblue/awtmod/RedBlueAWTMod.java
@@ -0,0 +1,8 @@
+package com.redandblue.awtmod;
+
+import net.minecraftforge.fml.common.Mod;
+
+@Mod(RedBlueAWTMod.MODID)
+public class RedBlueAWTMod {
+    public static final String MODID = "awtmod";
+}
